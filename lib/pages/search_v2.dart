427782585b31b5d8c8ff01f1e90824e021cc1e4a
pulnip <choiwh1008@gmail.com>
2025-03-14 10:51:01 +0900

    retrieve suggestions from supabase

--- lib/pages/search_v2.dart
+++ lib/pages/search_v2.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:my_ice_box/components/search_text.dart';
+import 'package:my_ice_box/main.dart';
+import 'package:my_ice_box/pages/search.dart';
+import 'package:my_ice_box/widgets/custom_future_builder.dart';
+import 'package:provider/provider.dart';
 
 class DataSearch extends SearchDelegate<String> {
-
-
-  final recentSearch = [
-    'apple',
-    'banana',
-    'orange',
-    'grape',
-    'watermelon'
-  ];
-
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
@@ -37,20 +31,36 @@ class DataSearch extends SearchDelegate<String> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    final suggestion = recentSearch.where(
-      (element) => element.startsWith(query)
-    ).toList();
-    
-    return ListView.builder(
-      itemBuilder: (context, index) => ListTile(
-        leading: const Icon(Icons.search),
-        onTap: () => showResults(context),
-        title: SearchText(
-          text: suggestion[index],
-          matchedIndex: query.length,
-        ),
-      ),
-      itemCount: suggestion.length,
+    return CustomFutureBuilder(
+      future: context.read<MyAppState>().supabase
+        .from('items')
+        .select('*')
+        .ilike('ingredient_name', '$query%')
+        .limit(10),
+      builder: (context, snapshot) {
+        return ListView.builder(
+          itemCount: snapshot.length,
+          itemBuilder: (context, index) {
+            final item = snapshot[index];
+
+            return ListTile(
+              leading: const Icon(Icons.search),
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ItemDetailPage(
+                    item: item,
+                  ),
+                ),
+              ),
+              title: SearchText(
+                text: item['ingredient_name'] as String,
+                matchedIndex: query.length,
+              ),
+            );
+          },
+        );
+      },
     );
   }
 
