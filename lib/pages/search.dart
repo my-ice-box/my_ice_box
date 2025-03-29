9a0bec9d4a8241dc77e0977d41294c98340f3e99
pulnip <choiwh1008@gmail.com>
2025-03-29 19:57:03 +0900

    improve search query

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -4,6 +4,7 @@ import 'package:my_ice_box/main.dart';
 import 'package:my_ice_box/pages/item_detail.dart';
 import 'package:my_ice_box/widgets/custom_future_builder.dart';
 import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class DataSearch extends SearchDelegate<String> {
   List<Map<String, dynamic>> cache = [];
@@ -31,6 +32,8 @@ class DataSearch extends SearchDelegate<String> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    final supabase = context.read<MyAppState>().supabase;
+
     Widget fetchSuggestions() {
       return ListView.builder(
         itemCount: cache.length,
@@ -49,16 +52,53 @@ class DataSearch extends SearchDelegate<String> {
       );
     }
 
-    return CustomFutureBuilder(
-      future: context.read<MyAppState>().supabase
-        .from('items')
+    Future<PostgrestList> searchSuggestions() async {
+      final results = await supabase.from('items')
         .select('ingredient_name')
-        .ilike('ingredient_name', '$query%')
-        .limit(10),
+        .ilike('ingredient_name', '%$query%')
+        .limit(20);
+
+      const Map<String, List<String>> choseongRange = {
+        'ㄱ': ['가', '깋'],
+        'ㄴ': ['나', '닣'],
+        'ㄷ': ['다', '딯'],
+        'ㄹ': ['라', '맇'],
+        'ㅁ': ['마', '밓'],
+        'ㅂ': ['바', '빟'],
+        'ㅅ': ['사', '싷'],
+        'ㅇ': ['아', '잏'],
+        'ㅈ': ['자', '짛'],
+        'ㅊ': ['차', '칳'],
+        'ㅋ': ['카', '킿'],
+        'ㅌ': ['타', '팋'],
+        'ㅍ': ['파', '핗'],
+        'ㅎ': ['하', '힣'],
+      };
+
+      final lastIndex = query.isNotEmpty ? query.length-1 : 0;
+      final lastChar = query[lastIndex];
+      final prefix = query.substring(0, lastIndex);
+
+      if(choseongRange.containsKey(lastChar)){
+        final range = choseongRange[lastChar]!;
+
+        final choseongMatch = await supabase.from('items')
+          .select('ingredient_name')
+          .gte('ingredient_name', prefix + range[0])
+          .lte('ingredient_name', prefix + range[1])
+          .limit(20);
+        results.addAll(choseongMatch);
+      }
+
+      return results;
+    }
+
+    return CustomFutureBuilder(
+      future: searchSuggestions(),
       onLoading: fetchSuggestions(),
       builder: (context, snapshot) {
+        cache = snapshot;
         if(snapshot.isNotEmpty) {
-          cache = snapshot;
         }
         return fetchSuggestions();
       },
